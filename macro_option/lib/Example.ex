e2dd083fb1c5b07174ce3f7a5bd892c3a2af590f
EGLIK <eglik@odin.vision>
2019-08-13 11:33:33 +0900

    test: ExDoc

--- macro_option/lib/Example.ex
+++ macro_option/lib/Example.ex
@@ -1,3 +1,14 @@
 defmodule Example do
+  @moduledoc """
+  Macro option example test
+  """
+
   use Hello, greeting: "Hola"
+
+  @doc """
+  test function doc
+  """
+  def test(string) do
+    String.upcase(string)
+  end
 end
--- macro_option/test/macro_option_test.exs
+++ macro_option/test/macro_option_test.exs
@@ -1,3 +1,6 @@
 defmodule MacroOptionTest do
   Example.hello("Sean") |> IO.puts
+  Example.test("uppercase!") |> IO.puts
+  String.length("가") |> IO.puts
+  byte_size("가") |> IO.puts
 end
